c958e819ba0def9197c7965b48bbf95d586caccf
Diomidis Spinellis <dds@aueb.gr>
2016-09-22 10:53:38 +0300

    Adopt Apache license
    The original license had open source terms, but GitHub could not
    list it as open source.

--- src/gr/spinellis/ckjm/MetricsFilter.java
+++ src/gr/spinellis/ckjm/MetricsFilter.java
@@ -1,17 +1,17 @@
 /*
- * $Id: \\dds\\src\\Research\\ckjm.RCS\\src\\gr\\spinellis\\ckjm\\MetricsFilter.java,v 1.9 2005/08/10 16:53:36 dds Exp $
- *
  * (C) Copyright 2005 Diomidis Spinellis
  *
- * Permission to use, copy, and distribute this software and its
- * documentation for any purpose and without fee is hereby granted,
- * provided that the above copyright notice appear in all copies and that
- * both that copyright notice and this permission notice appear in
- * supporting documentation.
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
  *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
- * MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  */
 
 package gr.spinellis.ckjm;
